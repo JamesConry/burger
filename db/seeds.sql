5d2a33ae69d807be60d1cd0db45c99de8c53ef6b
James Conry <james.conry@comcast.net>
2019-10-29 19:10:28 -0500

    basic functionality

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger1', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger2', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger3', False);
